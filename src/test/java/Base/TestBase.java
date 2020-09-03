cef2f3e9baadd7b978f312d375b7bda30421713b
truptibankar <truptibankar12@gmail.com>
2020-09-03 19:54:17 +0530

    EnhancedCode

--- src/test/java/Base/TestBase.java
+++ src/test/java/Base/TestBase.java
@@ -1,12 +1,31 @@
 package Base;
 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestBase {
 	
 	protected WebDriver driver; 
     protected final static String base_url = "https://amazon.in";
     protected final static int implicit_wait_timeout_in_sec = 20;
+    
+    @Before
+    public void set_up(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(implicit_wait_timeout_in_sec, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void clean_up(){
+        driver.quit();
+    }
+
+
 
 
 }
